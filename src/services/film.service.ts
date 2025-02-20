55cb1d10152f4a3546337fbc3ce9c695536f03bb
Arif Rahman <arif.rahman.rhm@gmail.com>
2025-02-20 14:31:09 +0700

    feat : without revalidate

--- src/services/film.service.ts
+++ src/services/film.service.ts
@@ -4,10 +4,7 @@ import { Film } from "@/types/film";
 export const FilmService = {
   getFilms: async (): Promise<Film[]> => {
     try {
-      const res = await fetch('https://swapi.dev/api/films/', {
-        next: { revalidate: 3600 }
-      });
-      
+      const res = await fetch('https://swapi.dev/api/films/');
       if (!res.ok) throw new Error('Failed to fetch films');
       
       const data = await res.json();
